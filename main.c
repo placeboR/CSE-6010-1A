44a0a2ef7dc60d54f9c50bc038b674cdd1ad97fb
HjR <R@HjRdeMacBook-Air.local>
2018-08-25 21:48:25 -0400

    modified to match the requirement

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
  *  n - the number of matrix columns
  *  p - the possibility of a cell is written as '1'
  *  A - the original m*n matrix filled with random '1's and '0's
- *  B - the modified m*n matrix according to the rules
+ *  tmp - temporary storing the modified m*n matrix according to the rules
  *  i - the counter for rows
  *  j - the counter for columns
  *
@@ -25,7 +25,7 @@
 
 int main(int argc, const char* argv[]){
     int A[m][n] = {0};	/* original matrix initialization */
-    int B[m][n] = {0};	/* modified matrix initialization */
+    int tmp[m][n] = {0};	/* temporary storing the modified matrix initialization */
     
     int i,j = 0;	/* counters initialization */
     
@@ -56,20 +56,21 @@ int main(int argc, const char* argv[]){
         for (j = 0; j < n; j++) {
             if (A[i][j] == 1) {
                 for (cnt = 0; cnt < m; cnt++) {
-                    B[cnt][j] = 1;  /* if there is a '1' in any cell of column j, all cells in the entire column should be set to 1 */
+                    tmp[cnt][j] = 1;  /* if there is a '1' in any cell of column j, all cells in the entire column should be set to 1 */
                 }
                 for (cnt = 0; cnt < n; cnt++) {
-                    B[i][cnt] = 1;  /* if there is a '1' in any cell of row i, all cells in the entire row should be set to 1 */
+                    tmp[i][cnt] = 1;  /* if there is a '1' in any cell of row i, all cells in the entire row should be set to 1 */
                 }
             }
         }
     }
     
-    /* print the modified matrix */
-    printf("\n\nThe modified matrix:\n\n");
+    /* print the resulting matrix */
+    printf("\n\nThe resulting matrix:\n\n");
     for(i = 0;i < m;i++){
         for(j = 0;j < n;j++){
-            printf("%d ",B[i][j]);  /* place a single space between '0's and '1's to enhance readability */
+            A[i][j] = tmp[i][j];    /* update the original matrix */
+            printf("%d ",A[i][j]);  /* place a single space between '0's and '1's to enhance readability */
         }
         printf("\n");
     }
